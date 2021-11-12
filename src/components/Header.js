98f68d00742f196462a75684da19f332748efed5
urmala99 <53155350+urmala99@users.noreply.github.com>
2021-11-12 06:58:31 +0200

    First commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React from "react";
 import styles from './Header.module.css';
 
+
 export default function Header(){
     return(
-        <div className= { styles.header }>
-            Foodcrosoft
+        <div className= { styles.container }>
+           <div className={ styles.logo }> <img src = 'logo.PNG' alt = '' width= '150' height= '70' /> </div>
+            <button className={ styles.login }>
+                Login
+            </button>
+            <div className={ styles.cart }>
+                <img src = 'cart.PNG' alt = '' width= '20' height= '20'/>
+            </div>
         </div>
     )
 }
