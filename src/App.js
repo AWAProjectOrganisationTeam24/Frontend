495d492989ca70106e863113d7b818f086cb49cf
JuliePrazakova <80099262+JuliePrazakova@users.noreply.github.com>
2021-11-16 17:26:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,26 +9,28 @@ import ProductMaker from './components/ProductMaker';
 
 
 class App extends React.Component {
+
   render(){
+
   return (
     <BrowserRouter>
       <div>
         <div className= 'header'>
-          <Link to= '/'><div>Foodcrosoft</div></Link>
-        </div>
+        <Link to= '/'><div>Foodcrosoft</div></Link>
+      </div>
       <Routes>
           <Route>
-        <Route path="/" element={ <Header/> }/>
-        <Route path="/login" element={ <Login/> }/>
-        <Route path="/login/createUser" element={ <CreateUser/> }/>
+            <Route path="/" element={ <Header/> } />
+            <Route path="/login" element={ <Login/> }/>
+            <Route path="/login/createUser" element={ <CreateUser/> }/>
           </Route>
           <Route>
-          <Route path='/restaurantMaker' element={<RestaurantMaker/>}/>
-          <Route path='/productMaker' element={<ProductMaker/>}/>
+            <Route path='/restaurantMaker' element={ <RestaurantMaker/> }/>
+            <Route path='/productMaker' element={ <ProductMaker/> }/>
           </Route>
         </Routes>
-        </div>
-      </BrowserRouter>
+      </div>
+    </BrowserRouter>
       
   );
 }}
