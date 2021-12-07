6f50be3a39ee005dd365b9800417362b4b0531b0
JuliePrazakova <80099262+JuliePrazakova@users.noreply.github.com>
2021-12-07 14:22:52 +0200

    Update CreateUser.js

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,46 +1,36 @@
-
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import axios from "axios";
 import styles from './Login.module.css';
 
+function CreateUser() {
+    const [state, setState] = React.useState({
+        firstname: '',
+        lastname: '',
+        mail: '',
+        psw: '',
+        address: '',
+        city: ''
+    })
 
-class CreateUser extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            firstname: '',
-            lastname: '',
-            mail: '',
-            psw: '',
-            address: '',
-            city: ''
-        }
+    function changeHandler(e) {
+        const value = e.target.value;
+        setState({
+            ...state,
+            [e.target.name]: value
+        });
     }
-
-    changeHandler = e => {
-        this.setState({
-            firstname: e.target.value[0],
-            lastname: e.target.value[1],
-            mail: e.target.value[2],
-            psw: e.target.value[3],
-            address: e.target.value[4],
-            city: e.target.value[5]
-        })
-    }
-
-    submitHandler = e => {
+    function submitHandler(e) {
         e.preventDefault();
 
         //send it to backend + ensure if goes bad
         //get response from backend - after login show homepage
-        axios.post("http://localhost:5000/", {
-            firstname: this.state.firstname,
-            lastname: this.state.lastname,
-            mail: this.state.mail,
-            psw: this.state.psw,
-            address: this.state.address,
-            city: this.state.city
+        axios.post("http://localhost:5000/register", {
+            firstname: state.firstname,
+            lastname: state.lastname,
+            mail: state.mail,
+            psw: state.psw,
+            address: state.address,
+            city: state.city
         })
             .then(res => {
                 console.log(res)
@@ -49,67 +39,59 @@ class CreateUser extends Component {
                 console.log(err)
             })
     }
-
-    render() {
-        const { firstname, lastname, mail, psw, address, city } = this.state
         return (
             <div>
-                                            
-
-            <div className ={ styles.container }>
-
-                <h1 className={styles.h1}>Create User</h1>
-
-                <form onSubmit={this.submitHandler}>
-                    <table>
-                        <tr>
-                            <td>Firstname</td>
-                            <input type="text"
-                                   name="firstname"
-                                   value={firstname}
-                                   onChange={this.changeHandler} />
-                        </tr>
-                        <tr>
-                            <td>Lastname</td>
-                            <input type="text"
-                                   name="lastname"
-                                   value={lastname}
-                                   onChange={this.changeHandler} />
-                        </tr>
-                        <tr>
-                            <td>Mail</td>
-                            <input type="text"
-                                   name="mail"
-                                   value={mail}
-                                   onChange={this.changeHandler} />
-                        </tr>
-                        <tr>
-                            <td>Password</td>
-                            <input type="text"
-                                   name="psw"
-                                   value={psw}
-                                   onChange={this.changeHandler} />
-                        </tr>
-                        <tr>
-                            <td>Address</td>
-                            <input type="text"
-                                   name="address"
-                                   value={address}
-                                   onChange={this.changeHandler} />
-                        </tr>
-                        <tr>
-                            <td>City</td>
-                            <input type="text"
-                                   name="city"
-                                   value={city}
-                                   onChange={this.changeHandler} />
-                        </tr>
-                    </table>
-                    <button type="submit">Create user</button>
-                </form>
-            </div>
+                <div className={styles.container}>
+                    <h1 className={styles.h1}>Create User</h1>
+                    <form onSubmit={submitHandler}>
+                        <table>
+                            <tr>
+                                <td>Firstname</td>
+                                <input type="text"
+                                       name="firstname"
+                                       value={state.firstname}
+                                       onChange={changeHandler}/>
+                            </tr>
+                            <tr>
+                                <td>Lastname</td>
+                                <input type="text"
+                                       name="lastname"
+                                       value={state.lastname}
+                                       onChange={changeHandler}/>
+                            </tr>
+                            <tr>
+                                <td>Mail</td>
+                                <input type="text"
+                                       name="mail"
+                                       value={state.mail}
+                                       onChange={changeHandler}/>
+                            </tr>
+                            <tr>
+                                <td>Password</td>
+                                <input type="text"
+                                       name="psw"
+                                       value={state.psw}
+                                       onChange={changeHandler}/>
+                            </tr>
+                            <tr>
+                                <td>Address</td>
+                                <input type="text"
+                                       name="address"
+                                       value={state.address}
+                                       onChange={changeHandler}/>
+                            </tr>
+                            <tr>
+                                <td>City</td>
+                                <input type="text"
+                                       name="city"
+                                       value={state.city}
+                                       onChange={changeHandler}/>
+                            </tr>
+                        </table>
+                        <button type="submit">Create user</button>
+                    </form>
+                </div>
             </div>
         )
-    }}
-export default CreateUser;
-
+}
+export default CreateUser
